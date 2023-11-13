d78377b33eec4f3625b228c4c4ebd707a8fb03ea
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-11-13 08:07:29 +0700

    create user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+// Protect all routes after this middleware
+
+router.route('/').get(userController.getAllUsers);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
