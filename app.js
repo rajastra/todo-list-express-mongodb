80d58666aa8d0b46e0cd035c509e2f8f8e397dae
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-11-13 08:06:00 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitze = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
+const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errController');
+
+const app = express();
+// global middleware
+
+// set security http headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+
+// limit requests from same API
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitze());
+
+// data sanitization against XSS
+app.use(xss());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Routes
+app.use('/api/v1/users', userRouter);
+
+app.use('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
